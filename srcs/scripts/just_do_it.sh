ff97228d4b063b122593b157c968464a21ef34b2
Arthur Gossuin <artygo@localhost.localdomain>
2020-02-21 10:23:29 +0100

    some work on fedora

--- srcs/scripts/just_do_it.sh
+++ srcs/scripts/just_do_it.sh
@@ -3,9 +3,9 @@ apt update
 apt upgrade -y
 apt install -y sudo
 apt install -y nginx
-apt install -y php7.3-fpm php-curl php-gd php-intl php-mbstring \
+apt install -y php7.3-fpm php-curl php-intl php-mbstring \
 	php-soap php-xml php-xmlrpc php-zip php7.3-cli php7.3-mysql php7.3-gd \
-	php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc
+	php7.3-imagick php7.3-recode php7.3-tidy
 
 chmod -R 755 /var/www/html/*
 chown -R www-data:www-data /var/www/html/*
@@ -15,9 +15,9 @@ nginx -t
 service nginx start
 
 COPY /srcs/mkcert mkcert
-chmod +x ./mkcert
-./mkcert -install
-./mkcert localhost.com
+sudo ./mkcert -install
+sudo ./mkcert -cert-file /etc/ssl/certs/localhost.crt \
+	-key-file /etc/ssl/certs/localhost.key localhost 127.0.0.1 localhost.com
 
 apt -y install mariadb-server mariadb-client
 service mysql start
