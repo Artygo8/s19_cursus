9233704bd30ace147541f0374986a939fc00a727
Artygo8 <arthur.gossuin@gmail.com>
2020-08-17 16:48:32 +0200

    default

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -11,16 +11,36 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+
 
 int main(int argc, char const *argv[])
 {
 	if (argc == 4)
 	{
-		
+		std::string to_replace = argv[2];
+		std::string line;
+		std::string text;
+		std::ifstream input_file(argv[1]);
+		std::ofstream output_file((std::string)argv[1] + (std::string)".replace");
+
+		while(getline(input_file, line))
+			text += line + "\n";
+		for (int i=0; i < text.size(); i++)
+		{
+			if (!(text.substr(i)).find(to_replace))
+			{
+				i += to_replace.size() - 1;
+				output_file << argv[3];
+			}
+			else
+				output_file << text[i];
+		}
 	}
 	else
-	{
-
-	}
+		std::cout << "Wrong Number of arguments\nUsage: ./replace FILENAME TO_REPLACE REPLACEMENT" << '\n';
 	return 0;
 }
