b143a91b07031146fa49a880d7c7fb3c79878682
agossuin <agossuin@student.s19.be>
2020-09-26 18:40:06 +0200

    the killerscript is way better

--- killer.sh
+++ killer.sh
@@ -8,30 +8,60 @@
 DEPLOY=$1
 APP=$1
 
+# Config
+ftps_app="pure-ftpd"
+wp_app="lighttpd"
+pma_app="lighttpd"
+
+HELP="USAGE : ./killer.sh   ftps | grafana | influxdb | mysql | wordpress (wp) | phpmyadmin (pma) | nginx | telegraf | sshd"
+
 function apply {
-	FUNCTION="kubectl exec deploy/${DEPLOY}-deployment -- pkill $APP"
-	echo $FUNCTION
-	$FUNCTION
+	if [ $APP == "telegraf" ];then	
+		for i in "nginx" "ftps" "grafana" "mysql" "phpmyadmin" "wordpress"; do
+			DEPLOY=$i
+			FUNCTION="kubectl exec deploy/${DEPLOY}-deployment -- pkill $APP"
+			echo $FUNCTION
+			$FUNCTION
+		done
+	else
+		FUNCTION="kubectl exec deploy/${DEPLOY}-deployment -- pkill $APP"
+		echo $FUNCTION
+		$FUNCTION
+	fi
 }
 
-if [ $APP == "telegraf" ]; then
-	for i in {"nginx" "ftps" "grafana" "mysql" "phpmyadmin" "wordpress"}; do
-		APP=$i
-		apply
-	done
+if [ $# == 0 ]; then
+	echo $HELP
+	echo
+	echo "remove 1 at the time then verify that the pod restarts with kubectl get pods"
+	echo
+	kubectl get pods
 	exit
 fi
 
 if [ $APP == "sshd" ]; then
 	DEPLOY="nginx"
-fi
 
-if [ $APP == "ftps" ]; then
-	APP=pure-ftpd
-fi
+elif [ $APP == "influxdb" ]; then
+	APP="influxd"
 
-apply
+elif [ $APP == "ftps" ]; then
+	APP=$ftps_app
+
+elif [ $APP == "wp" ] || [ $APP == "wordpress" ]; then
+	APP=$wp_app
+	DEPLOY="wordpress"
+elif [ $APP == "pma" ] || [ $APP == "phpmyadmin" ]; then
+	APP=$pma_app
+	DEPLOY="phpmyadmin"
+fi
 
 if [ -e ./nc_test.sh ]; then
 	./nc_test.sh
+	tput setaf 3
+	apply
+	tput sgr0
+	./nc_test.sh
+else
+	apply
 fi
