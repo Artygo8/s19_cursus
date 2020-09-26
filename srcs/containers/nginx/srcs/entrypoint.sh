0832d957ae2e60767e5bc6e924c211d168771ff3
agossuin <agossuin@student.s19.be>
2020-09-26 17:44:13 +0200

    i fcked up my entrypoint with && in the while

--- srcs/containers/nginx/srcs/entrypoint.sh
+++ srcs/containers/nginx/srcs/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 /etc/init.d/sshd restart
-(telegraf && pkill nginx)&
+(telegraf; pkill nginx)&
 
 function _monitor {
-	while [ -e run/sshd.pid && ]; do
+	sleep 1
+	while [ -e run/sshd.pid ]; do
     		sleep 3
 	done
-	killall nginx
+	pkill nginx
 }
 
 _monitor &
