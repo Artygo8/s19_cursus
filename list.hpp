e8e417953181f28f8a37b6007d94965caa46941d
Artygo8 <arthur.gossuin@gmail.com>
2020-12-29 10:13:01 +0100

    hello

--- list.hpp
+++ list.hpp
@@ -18,52 +18,147 @@
 # include <list>
 # include <memory>
 
-std::list<int> l;
+namespace ft { template < class T > class list; };
 
-namespace ft
+//  .o88o.     .           oooo   o8o               .   
+//  888 `"   .o8           `888   `"'             .o8   
+// o888oo  .o888oo          888  oooo   .oooo.o .o888oo 
+//  888      888            888  `888  d88(  "8   888   
+//  888      888   o8o o8o  888   888  `"Y88b.    888   
+//  888      888 . `"' `"'  888   888  o.  )88b   888 . 
+// o888o     "888" o8o o8o o888o o888o 8""888P'   "888" 
+//                 `"' `"'                              
+
+template < class T > class ft::list
 {
-	template < class T >
-	class list;
-}
 
-template < class T >
-class ft::list
-{
-	private:
+//                      o8o                            .             
+//                      `"'                          .o8             
+// oo.ooooo.  oooo d8b oooo  oooo    ooo  .oooo.   .o888oo  .ooooo.  
+//  888' `88b `888""8P `888   `88.  .8'  `P  )88b    888   d88' `88b 
+//  888   888  888      888    `88..8'    .oP"888    888   888ooo888 
+//  888   888  888      888     `888'    d8(  888    888 . 888    .o 
+//  888bod8P' d888b    o888o     `8'     `Y888""8o   "888" `Y8bod8P' 
+//  888                                                              
+// o888o                                                             
 
-		struct node
-		{
-			T		data;
+private:
+
+//               |     
+// ,---.,---.,---|,---.
+// |   ||   ||   ||---'
+// `   '`---'`---'`---'
+
+	class node
+	{
+		public:
+			T		&content;
 			node	*next;
 			node	*previous;
-		};
-
-		size_t		size;
-		node		*head;
-		node		*tail;
-	
-	public:
-		// (constructor)
-		explicit list () : size(0), head(NULL), tail(NULL) {}
-
-		explicit list (size_t n, const T & val = 0)
-		{
 			
-		}
+			node(T & content, node *previous, node *next) : content(content), previous(previous), next(next) {}
+			node & operator =(const node & other)
+			{
+				this->content = other.content;
+				this->next = other.next;
+				this->previous = other.previous;
+				return *this;
+			}
 
-		template <class InputIterator>
-		list (InputIterator first, InputIterator last)
+			void add_back(node & new_node)
+			{
+				node *last;
+				last = this;
+				while (last->next)
+					last = last->next;
+				new_node.previous = last;
+				last->next = &new_node;
+			}
+	};
+
+
+//                 o     |    |              
+// .    ,,---.,---..,---.|---.|    ,---.,---.
+//  \  / ,---||    |,---||   ||    |---'`---.
+//   `'  `---^`    ``---^`---'`---'`---'`---'
+
+	size_t		size;
+	node		*head;
+	node		*tail;
+
+//                         .o8       oooo   o8o            
+//                        "888       `888   `"'            
+// oo.ooooo.  oooo  oooo   888oooo.   888  oooo   .ooooo.  
+//  888' `88b `888  `888   d88' `88b  888  `888  d88' `"Y8 
+//  888   888  888   888   888   888  888   888  888       
+//  888   888  888   888   888   888  888   888  888   .o8 
+//  888bod8P'  `V88V"V8P'  `Y8bod8P' o888o o888o `Y8bod8P' 
+//  888                                                    
+// o888o                                                   
+
+public:
+
+// o|                   |              
+// .|--- ,---.,---.,---.|--- ,---.,---.
+// ||    |---'|    ,---||    |   ||    
+// ``---'`---'`    `---^`---'`---'`    
+
+	class iterator
+	{
+		private:
+			node &current;
+		
+		public:
+			// COPLIEN
+			iterator (node & current) : current(current) {}
+			iterator (const iterator & other) : current(other.current) {}
+			iterator operator= (const iterator & other)
+			{
+				this->current = other.current;
+				return *this;
+			}
+
+			bool	operator== (const iterator & rhs) { return this->current.content == rhs.current.content; }
+			bool	operator!= (const iterator & rhs) { return this->current.content != rhs.current.content; }
+			T &		operator* () { return this->current.content; }
+			void	operator++ () { this->current = *(this->current.next); }
+			void	operator++ (int) { this->current = *(this->current.next); }
+			void	operator-- () { this->current = *(this->current.previous); }
+	};
+
+//                     |                   |              
+// ,---.,---.,---.,---.|--- ,---..   .,---.|--- ,---.,---.
+// |    |   ||   |`---.|    |    |   ||    |    |   ||    
+// `---'`---'`   '`---'`---'`    `---'`---'`---'`---'`    
+
+	explicit list () : size(0), head(nullptr), tail(nullptr) {}
+
+	explicit list (size_t n, const T & val = 0) : size(n)
+	{
+		for (size_t i = 0; i < n; i++)
 		{
-			(void) first;
-			(void) last;
+			T tmp_val(val);
+			node new_node(tmp_val, nullptr, nullptr);
+			head->add_back(new_node);
+			tail = new_node;
 		}
+	}
 
-		list (const list & x)
-		{
-			(void) x;
-		}
+	template <class InputIterator>
+	list (InputIterator first, InputIterator last)
+	{
+		size = 0;
+		(void) first;
+		(void) last;
+	}
 
+	list (const list & x)
+	{
+		(void) x;
+	}
 
+	iterator begin() { return iterator(*head); }
+	iterator end() { return iterator(*tail); }
 
 };
 
--- main.cpp
+++ main.cpp
@@ -68,19 +68,24 @@ int main()
 		// constructors used in the same order as described above:
 		ft::list<int> first;                                // empty list of ints
 		ft::list<int> second (4,100);                       // four ints with value 100
-		ft::list<int> third (second.begin(),second.end());  // iterating through second
-		ft::list<int> fourth (third);                       // a copy of third
+		// ft::list<int> third (second.begin(),second.end());  // iterating through second
+		// ft::list<int> fourth (third);                       // a copy of third
 
 		// the iterator constructor can also be used to construct from arrays:
-		int myints[] = {16,2,77,29};
-		ft::list<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+		// int myints[] = {16,2,77,29};
+		// ft::list<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
 
 		cout << "The contents of fifth are: ";
-		for (ft::list<int>::iterator it = fifth.begin(); it != fifth.end(); it++)
-			cout << *it << ' ';
+		// for (ft::list<int>::iterator it = second.begin(); it != second.end(); it++)
+		ft::list<int>::iterator it = second.begin();
+		cout << *it << ' ';
 		cout << '\n';
 	}
 
+	{
+		int a(5);
+		cout << a << endl;
+	}
 
 	return 0;
 }
