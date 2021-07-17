2bdbd62a7ed232b4c0dc902729ed32edd1285844
Artygo8 <arthur.gossuin@gmail.com>
2021-07-17 10:24:03 +0200

    merge_in_subdirectories mini-script

---
+++ merge_in_subdirectories.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PREFIX="s19_"
+FILE="repo_name.txt"
+REPO="https://github.com/Artygo8/"
+
+
+if [[ -e $FILE ]];then
+	cat $FILE |while read line; do
+		if [[ -n $line ]];then
+  			echo $PREFIX$line.git
+			git remote add -f $line $REPO$PREFIX$line.git
+			git merge -s ours --no-commit --allow-unrelated-histories $line/master
+			git read-tree --prefix=$line/ -u $line/master
+			git add --all
+			git commit -m "merging $PREFIX$line into subdirectory $line"
+		fi
+	done
+else
+	echo "/!\\ Please create a file named $FILE with the names of your repositories. These will be prefixed with \"$PREFIX\"."
+fi
+
